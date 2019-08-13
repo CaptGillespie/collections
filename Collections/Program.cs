2730fd14f0a7214d74b5d6e8c88c4f822e97ef93
Sean Gillespie <sean.gillespie22@gmail.com>
2019-08-13 07:49:25 -0700

    first attempt

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object>stuff = new List<object>();
+            int sum = 0;
+            stuff.Add(7); 
+            stuff.Add(28);
+            stuff.Add(-1);
+            stuff.Add(true);
+            stuff.Add("chair");
+            foreach (object things in stuff)
+            {
+                Console.WriteLine(things);
+                if (things is int)
+                {
+                    sum += (int)things;
+                }
+                Console.WriteLine(sum);
+            }
+            
+        }
+    }
+}
